63a6aa5e1fbb74fe5dc9f5934ae0e88336df98c9
Vinh Do <vinhdo07@gmail.com>
2019-02-28 02:16:34 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,22 +12,15 @@ class Triangle
 
   def kind
     array = [@l1, @l2, @l3].sort
+    equality = array[0] + array[1] > array[2]
     if array.any?{|l| l <= 0 || l == nil}
       raise TriangleError
     elsif array.uniq.size == 1
       @kind = :equilateral
-    elsif array.uniq.size == 2
-      if array[0] + array[1] > array[2]
-        @kind = :isosceles
-      else
-        raise TriangleError
-      end
-    elsif array.uniq.size == 3
-      if array[0] + array[1] > array[2]
-        @kind = :scalene
-      else
-        raise TriangleError
-      end
+    elsif array.uniq.size == 2 && equality
+      @kind = :isosceles
+    elsif array.uniq.size == 3 && equality
+      @kind = :scalene
     else
       raise TriangleError
     end #End of if/else nest
