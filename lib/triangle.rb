ffdea73b89de4f6764f638b23608c70d154f3dd3
Joan Lyness <joanrigdon@gmail.com>
2019-02-28 16:03:51 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,13 +11,17 @@ class Triangle
 
   def valid?
     if @sides.any? {|side| side <= 0}
+      # [1] pry(#<Triangle>)> @sides=> [0, 0, 0]
+      # [2] pry(#<Triangle>)> @sides.any? {|side| side = 0}=> true
+      # [3] pry(#<Triangle>)> @sides.any? {|side| side <= 0}=> true
+      #binding.pry
       false
     elsif @sides[0] > @sides[1] + @sides[2] ||
       @sides[1] > @sides[0] + @sides[2] ||
       @sides[2] > @sides[0] +  @sides[1]
       false
-    #should wok in ruby 2.5 but does not work in 2.3.1
-    # elsif @sides.any? {|side| side > (@sides - side.to_s.split).sum}
+    #should work in ruby 2.5 but does not work in 2.3.1
+    # elsif @sides.any? {|side| side > (@sides - side.to_s.split[0].to_i).sum}
     #   false
     else
       true
@@ -38,6 +42,13 @@ class Triangle
       false
     end
   end
+
   # class TriangleError < StandardError
+  #   def i_must_have_truth(value)
+  #     if value == false
+  #       raise TypeError, "Invalid triangle!"
+  #     end
+  #   end
   # end
+
 end
