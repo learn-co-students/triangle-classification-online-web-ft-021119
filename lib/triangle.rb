b4aa4a59d7657c6d1a355b18cf738e3393634830
Jacqi Entzminger <jackie.entz@gmail.com>
2019-03-01 01:15:39 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+  attr_accessor :length_one, :length_two, :length_three
+  
+  def initialize(length_one, length_two, length_three)
+    @length_one = length_one
+    @length_two = length_two
+    @length_three = length_three
+  end 
+  
+  
+  def kind 
+    length_array = [length_one, length_two, length_three]
+    order = length_array.sort 
+    if order.include?(0) || order[0] + order[1] <= order[2]
+      raise TriangleError
+    else  
+      if order[0] == order[1] && order[1] == order[2] 
+        :equilateral
+        elsif order[0] != order[1] && order[1] == order[2]
+       :isosceles
+    else 
+      :scalene
+    end
+  end 
+  end 
+
+    
+    
+ 
+  
+  class TriangleError < StandardError
+    def message
+      puts "what in the isoterics is this???" 
+    end 
+  end 
+  
 end
