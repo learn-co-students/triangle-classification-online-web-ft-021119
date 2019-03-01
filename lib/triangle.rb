b3f4e425d1a1b7bf12e82d37bf8e4c8fbadb2daa
Christoph Gruener <christophgruener@me.com>
2019-03-01 19:40:18 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  attr_accessor :l_1, :l_2, :l_3
+
+
+
+  def initialize(l_1, l_2, l_3)
+    @l_1 = l_1
+    @l_2 = l_2
+    @l_3 = l_3
+  end
+
+  def kind
+
+    if @l_1 <= 0 || @l_2 <= 0 || @l_3 <= 0
+      raise TriangleError
+    elsif @l_1 + @l_2 <= @l_3 || @l_1 + @l_3 <= @l_2 || @l_2 + @l_3 <= @l_1
+      raise TriangleError
+    elsif @l_1 == @l_2 && @l_1 == @l_3 && @l_2 == @l_3
+      :equilateral
+    elsif @l_1 != @l_2 && @l_1 != @l_3 && @l_2 != @l_3
+      :scalene
+    elsif @l_1 == @l_2 || @l_1 == @l_3 || @l_2 == @l_3
+      :isosceles
+    end
+  end
+
+
+
+
+  class TriangleError < StandardError
+
+  end
+
+
+
 end
